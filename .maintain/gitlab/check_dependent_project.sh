e4e9d7f0a384bdbfe8d919904d155cb9c9e5f429
joao-paulo-parity <joao-paulo@parity.io>
2021-09-13 04:28:42 -0300

    use jq from CI

--- .maintain/gitlab/check_dependent_project.sh
+++ .maintain/gitlab/check_dependent_project.sh
@@ -24,12 +24,6 @@ changes.
 
 "
 
-# FIXME: use base jq from CI when it gets updated to Ubuntu LTS
-jq="$PWD/jq16"
-curl -sqL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o "$jq"
-chmod +x "$jq"
-echo "af986793a515d500ab2d35f8d2aecd656e764504b789b66d7e1a0b727a124c44  $jq" | sha256sum --check
-
 set -eu -o pipefail
 shopt -s inherit_errexit
 
