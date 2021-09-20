956007123e64eb08ff16945087c2a65d2f382c4c
Alexander Theißen <alex.theissen@me.com>
2021-09-20 12:25:05 +0200

    Add borrowing

--- frame/contracts/src/storage/meter.rs
+++ frame/contracts/src/storage/meter.rs
@@ -17,60 +17,102 @@
 
 //! This module contains functions to meter the storage usage.
 
-use crate::{Config, BalanceOf};
-use frame_support::dispatch::{DispatchResult, DispatchError};
+use crate::{BalanceOf, Config};
+use frame_support::{dispatch::{DispatchError, DispatchResult}, DefaultNoBound};
 use sp_core::crypto::UncheckedFrom;
+use sp_runtime::traits::Saturating;
 use sp_std::marker::PhantomData;
-use sp_runtime::traits::{Zero, Saturating};
 
-trait Handler<T: Config> {
+pub trait Ext<T: Config> {
 	fn reserve_limit(origin: &T::AccountId, limit: &BalanceOf<T>) -> DispatchResult;
+	fn charge(origin: &T::AccountId, contract: &T::AccountId) -> DispatchResult;
 }
 
-struct Meter<T: Config, H> {
-	origin: T::AccountId,
+pub struct Meter<'parent, T: Config, E> {
+	parent: Parent<'parent, T, E>,
 	contract: T::AccountId,
 	limit: BalanceOf<T>,
-	used: BalanceOf<T>,
+	total_usage: Usage<T>,
+	own_usage: Usage<T>,
+	_handler: PhantomData<E>,
+}
+
+pub enum Cost<T: Config> {
+	Charge(BalanceOf<T>),
+	Refund(BalanceOf<T>),
+}
+
+enum Parent<'parent, T: Config, E> {
+	Origin(T::AccountId),
+	Meter(&'parent mut Meter<'parent, T, E>),
+}
+
+impl<'parent, T: Config, E> Parent<'parent, T, E> {
+	fn as_meter(&'parent mut self) -> Option<&mut Meter<'parent, T, E>> {
+		match self {
+			Self::Meter(meter) => Some(meter),
+			Self::Origin(_) => None,
+		}
+	}
+
+	fn origin(&'parent self) -> &T::AccountId {
+		let mut current = self;
+		loop {
+			match current {
+				Self::Meter(meter) => current = &meter.parent,
+				Self::Origin(origin) => return &origin,
+			}
+		}
+	}
+}
+
+#[derive(DefaultNoBound, Clone)]
+struct Usage<T: Config> {
+	charged: BalanceOf<T>,
 	refunded: BalanceOf<T>,
-	_handler: PhantomData<H>,
 }
 
-pub enum Charge {
-	Charge(u32),
-	Refund(u32),
-}
-
-impl<T, H> Meter<T, H>
+impl<'parent, T, E> Meter<'parent, T, E>
 where
 	T: Config,
 	T::AccountId: UncheckedFrom<T::Hash> + AsRef<[u8]>,
-	H: Handler<T>,
+	E: Ext<T>,
 {
-	pub fn new(origin: T::AccountId, contract: T::AccountId, limit: BalanceOf<T>) -> Result<Self, DispatchError> {
-		H::reserve_limit(&origin, &limit)?;
+	pub fn new(
+		origin: T::AccountId,
+		contract: T::AccountId,
+		limit: BalanceOf<T>,
+	) -> Result<Self, DispatchError> {
+		E::reserve_limit(&origin, &limit)?;
 		Ok(Self {
-			origin,
+			parent: Parent::Origin(origin),
 			contract,
 			limit,
-			used: <BalanceOf<T>>::zero(),
-			refunded: <BalanceOf<T>>::zero(),
+			total_usage: Default::default(),
+			own_usage: Default::default(),
 			_handler: PhantomData,
 		})
 	}
 
-	pub fn nested(&mut self, contract: T::AccountId) -> Self {
+	pub fn nested(&'parent mut self, contract: T::AccountId) -> Self {
+		let limit = self.available();
+		let total_usage = self.total_usage.clone();
 		Self {
-			origin: self.origin.clone(),
+			parent: Parent::Meter(self),
 			contract,
-			limit: self.available(),
-			used: <BalanceOf<T>>::zero(),
-			refunded: <BalanceOf<T>>::zero(),
+			limit,
+			total_usage,
+			own_usage: Default::default(),
 			_handler: PhantomData,
 		}
 	}
 
+	pub fn absorb_nested(&'parent mut self) -> DispatchResult {
+		let parent = self.parent.as_meter().expect("Due to typestate this always a nested meter; qed");
+		unimplemented!()
+	}
+
 	fn available(&self) -> BalanceOf<T> {
-		self.limit.saturating_add(self.refunded).saturating_sub(self.used)
+		self.limit.saturating_add(self.total_usage.refunded).saturating_sub(self.total_usage.charged)
 	}
 }
